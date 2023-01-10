502886ccd44090e3b7c03e965eddcc7d4f8406d2
Anthonydt16 <@email>
2023-01-10 19:16:01 +0100

    recherche affichage contrat

--- src/components/Map.js
+++ src/components/Map.js
@@ -13,15 +13,16 @@ import {map} from "leaflet/src/map";
 const Map = () => {
     const [markersContrat, setMarkersContrat] = useState([]);
     const [markersStation, setMarkersStation] = useState([]);
+    const [markers, setMarkers] = useState([]);
     const [contrats, setContrats] = useState([]);
+    const [stations, setStations] = useState([]);
+
 
     useEffect(() => {
-
+        let marker = [];
         async function fetchStart() {
-            let markerContrat = [];
-            let markers = [];
+            let marker = [];
             let contratsJson;
-            let htmlMarket = "";
             try {
                 let contratss = await fetch("https://api.jcdecaux.com/vls/v1/contracts?apiKey=7886a12c53604b2668a08582a04795afcc9375b0");
                 contratsJson = await contratss.json();
@@ -42,44 +43,79 @@ const Map = () => {
                     contratFilter.filter((contrat) => contrat.name === contratUnit.name)['longitude'] = stationsJson[0].position.longitude;
                     contratFilter.filter((contrat) => contrat.name === contratUnit.name)['latitude'] = stationsJson[0].position.latitude;
                     setContrats(contratFilter);
+                    setStations(stationsJson);
                     if (!contratUnit.latitude || !contratUnit.longitude) {
                         console.log("pas de position");
                         return null;
                     }// ignorez les stations sans coordonnées
                     else {
-                        markers.push(<Marker position={[contratUnit.latitude, contratUnit.longitude]}><Popup><p>le nom: {contratUnit.name}</p><p>le pays: {contratUnit.country_code}</p><p>le nom commercial: {contratUnit.commercial_name}</p></Popup></Marker>);
+                        marker.push(<Marker key={contratUnit.name} position={[contratUnit.latitude, contratUnit.longitude]}><Popup><p>le nom: {contratUnit.name}</p><p>le pays: {contratUnit.country_code}</p><p>le nom commercial: {contratUnit.commercial_name}</p></Popup></Marker>);
                     }
-
                 }
+
             }else{
                 console.log("pas de contrat");
             }
             setContrats(contratsJson);
-            setMarkersContrat(markers);
+            setMarkersContrat(marker);
         }
         fetchStart();
     }, []);
 
+    useEffect(() => {
+        async function fetchStart() {
+            let marker = markersStation;
+            stations.map((station, key) => {
+                //ajouter sans les doublons
+                marker.push(
+                    <Marker key={key} position={[station.position.latitude, station.position.longitude]}>
+                        <Popup>
+                            <p>le nom: {station.name}</p>
+                            <p>adresse: {station.address}</p>
+                            <p>status: {station.status}</p>
+                            <p>total de vélos: {station.totalStands.availabilities.bikes}</p>
+                            <p>total de vélos électriques: {station.totalStands.availabilities.mechanicalBikes}</p>
+                            <p>total de vélos mécaniques: {station.totalStands.availabilities.electricalBikes}</p>
+                        </Popup>
+                    </Marker>
+                )
+
+            })
+            setMarkersStation(marker);
+        }
+        fetchStart();
+    }, [stations]);
+
 
     const position = [46.00, 2.00]
 
+    function contrat() {
+        console.log(markers)
+        console.log("contrat");
+        setMarkers(markersContrat);
+        console.log(markers);
+    }
 
+    function station() {
+        console.log(markers)
+        setMarkers(markersStation);
+        console.log(markers);
+    }
 
     return (
         <map>
             <NavBar/>
             <h2>La cartes de toutes les stations</h2>
             <div>
-                <span >Contrat</span>
-                <span >Station</span>
+                <span onClick={station}>Station</span>
+                <span onClick={contrat}>Contrat</span>
             </div>
-
             <MapContainer id={"map"} className={"map"} center={position} zoom={2} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {markersContrat}
+                {markers}
             </MapContainer>
         </map>
     );
--- src/pages/Recherche.js
+++ src/pages/Recherche.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Recherche = () => {
+    const [search, setSearch] = useState([]);
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        let searchs;
+        async function fetchStart() {
+            try {
+                searchs = await fetch("https://api.jcdecaux.com/vls/v1/contracts?apiKey=7886a12c53604b2668a08582a04795afcc9375b0");
+                searchs = await searchs.json();
+                searchs = searchs.filter((searchu) => searchu.country_code === "FR");
+                //faire un like sur searchs filter
+                searchs = searchs.filter((searchu) => searchu.name.toLowerCase().includes(search.toLowerCase()));
+            }catch (e) {
+                console.log(e);
+            }
+            setSearchResult(searchs);
+        }
+        fetchStart();
+    }, [search]);
     return (
         <div>
             Recherche
+            <input type="text" id="search-input" placeholder="Recherche" onChange={(e) => setSearch(e.target.value)}/>
+            <div className={"searchResult"}>
+                {searchResult.map((searchR, key) => {return (
+                    <div className={"card"} key={key}>
+                        <h2>{searchR.name}</h2>
+                        <span>{searchR.country_code}</span>
+                        <p>{searchR.commercial_name}</p>
+                        <a className={"button"} href={"/carte/" + searchR.name}>Voir les stations sur carte</a>
+                    </div>
+                )})}
+            </div>
         </div>
     );
 };
